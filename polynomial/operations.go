fca100d59e366c387991f381ab08f74a7e54f9ad
Manuel Pena Garcia <manuelpga@ext.inditex.com>
2021-01-03 16:02:31 +0100

    Adding more operations
    Adding Derive, Integrate and Evaluate

--- polynomial/operations.go
+++ polynomial/operations.go
@@ -1,16 +1,20 @@
 package polynomial
 
+import (
+	"math"
+)
+
 // Sum obtains a Polynomial representing the sum of the caller and other Polynomial
 func (p Polynomial) Sum(other Polynomial) Polynomial {
 	m := make(map[uint]float64)
 
 	grade := max(p.grade, other.grade)
 
-	for i := 0; i <= int(grade); i++ {
-		sum := p.coefficients[uint(i)] + other.coefficients[uint(i)]
+	for i := uint(0); i <= grade; i++ {
+		sum := p.coefficients[i] + other.coefficients[i]
 
-		if (sum-0.)*(sum-0.) > precission {
-			m[uint(i)] = sum
+		if checkNotZero(p.coefficients[i]) {
+			m[i] = sum
 		}
 	}
 
@@ -23,11 +27,11 @@ func (p Polynomial) Subtract(other Polynomial) Polynomial {
 
 	grade := max(p.grade, other.grade)
 
-	for i := 0; i <= int(grade); i++ {
-		sub := p.coefficients[uint(i)] - other.coefficients[uint(i)]
+	for i := uint(0); i <= grade; i++ {
+		sub := p.coefficients[i] - other.coefficients[i]
 
-		if (sub-0.)*(sub-0.) > precission {
-			m[uint(i)] = sub
+		if checkNotZero(sub) {
+			m[i] = sub
 		}
 	}
 
@@ -40,13 +44,13 @@ func (p Polynomial) Multiply(other Polynomial) Polynomial {
 
 	grade := p.grade + other.grade
 
-	for i := 0; i <= int(p.grade); i++ {
-		for j := 0; j <= int(other.grade); j++ {
-			a := p.coefficients[uint(i)]
-			b := other.coefficients[uint(j)]
+	for i := uint(0); i <= p.grade; i++ {
+		for j := uint(0); j <= other.grade; j++ {
+			a := p.coefficients[i]
+			b := other.coefficients[j]
 
-			if (a-0.)*(a-0.) > precission && (b-0.)*(b-0.) > precission {
-				m[uint(i+j)] += a * b
+			if checkNotZero(a * b) {
+				m[i+j] += a * b
 			}
 		}
 	}
@@ -54,9 +58,55 @@ func (p Polynomial) Multiply(other Polynomial) Polynomial {
 	return Polynomial{m, grade}
 }
 
+// Derive retrieves the derivative Polyinomial as a result of deriving the caller Polynomial
+func (p Polynomial) Derive() Polynomial {
+	m := make(map[uint]float64)
+
+	for i := uint(0); i <= p.grade; i++ {
+		if checkNotZero(p.coefficients[i+1]) {
+			m[i] = p.coefficients[i+1] * (float64(i + 1))
+		}
+	}
+
+	return Polynomial{m, p.grade - 1}
+}
+
+// Integrate retrieves the Polynomial result of the integration of the caller Polynomial
+// The argument will be the constant of integration
+func (p Polynomial) Integrate(constant float64) Polynomial {
+	m := make(map[uint]float64)
+
+	m[0] = constant
+
+	for i := uint(0); i <= p.grade+1; i++ {
+		if checkNotZero(p.coefficients[i]) {
+			m[i+1] = p.coefficients[i] / float64(i+1)
+		}
+	}
+
+	return Polynomial{m, p.grade + 1}
+}
+
+// Evaluate retrieves the value of the caller Polynomial expression in a given point x
+func (p Polynomial) Evaluate(x float64) float64 {
+	var sum float64
+
+	for i := uint(0); i <= p.grade; i++ {
+		if checkNotZero(p.coefficients[i]) {
+			sum += p.coefficients[i] * math.Pow(x, float64(i))
+		}
+	}
+
+	return sum
+}
+
 func max(a uint, b uint) uint {
 	if a > b {
 		return a
 	}
 	return b
+}
+
+func checkNotZero(a float64) bool {
+	return (a*a - 0.) > precission
 }
